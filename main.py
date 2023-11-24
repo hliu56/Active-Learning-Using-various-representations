f1df46b30b760ae13be1083aeb56cd5548f2990a
hliu56 <hliu56buffalo@gmail.com>
2023-11-24 10:16:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,21 +16,29 @@ RepeatTimes = 20
 X, y = get_input()
 
 def main():
+    # Run different algorithms to get model performance and labeled data pool
+    R2Smooth_std1, accuracySmooth1, InfoSmooth_std1, InfoSmooth_mean1,\
+    MSEsmooth_std1,MSEsmooth1,MAEsmooth_std1, MAEsmooth1,\
+    R2_train_std1, R2_train_mean1, R2_train_stdS1, R2_train_meanS1, SelectData1 = random(X, y, runs=RepeatTimes)
+    
     R2Smooth_std2, accuracySmooth2, InfoSmooth_std2, InfoSmooth_mean2,\
     MSEsmooth_std2,MSEsmooth2,MAEsmooth_std2, MAEsmooth2,\
-    R2_train_std2, R2_train_mean2, R2_train_stdS2, R2_train_meanS2, SelectData = Uncertainty(X, y, runs=3)
+    R2_train_std2, R2_train_mean2, R2_train_stdS2, R2_train_meanS2, SelectData2 = Uncertainty(X, y, runs=RepeatTimes )
     
     R2Smooth_std3, accuracySmooth3, InfoSmooth_std3, InfoSmooth_mean3,\
     MSEsmooth_std3,MSEsmooth3,MAEsmooth_std3, MAEsmooth3,\
-    R2_train_std3, R2_train_mean3, R2_train_stdS3, R2_train_meanS3, SelectData = GSx(X, y, runs=RepeatTimes)
+    R2_train_std3, R2_train_mean3, R2_train_stdS3, R2_train_meanS3, SelectData3 = GSx(X, y, runs=RepeatTimes)
 
     R2Smooth_std4, accuracySmooth4, InfoSmooth_std4, InfoSmooth_mean4,\
     MSEsmooth_std4,MSEsmooth4,MAEsmooth_std4, MAEsmooth4,\
-    R2_train_std4, R2_train_mean4, R2_train_stdS4, R2_train_meanS4, SelectData = GSy(X, y, runs=RepeatTimes)
+    R2_train_std4, R2_train_mean4, R2_train_stdS4, R2_train_meanS4, SelectData4 = GSy(X, y, runs=RepeatTimes)
     
     R2Smooth_std5, accuracySmooth5, InfoSmooth_std5, InfoSmooth_mean5,\
     MSEsmooth_std5,MSEsmooth5,MAEsmooth_std5, MAEsmooth5,\
-    R2_train_std5, R2_train_mean5, R2_train_stdS5, R2_train_meanS5, SelectData = iGS(X, y, runs=RepeatTimes)
+    R2_train_std5, R2_train_mean5, R2_train_stdS5, R2_train_meanS5, SelectData5 = iGS(X, y, runs=RepeatTimes)
+
+    # Plotting
+    ...
     
 
 
