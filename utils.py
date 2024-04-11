b740765d98a8606627c9a1c15c76672cfe3163ea
hliu56 <80417190+hliu56@users.noreply.github.com>
2024-04-11 10:09:50 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,11 +25,6 @@ def get_input(myfile):
     df_load['JSC']=df_load['JSC'].abs()
     df_reduce=df_load.iloc[:,3:]
     df_refine=df_reduce.iloc[:,np.r_[0:3,4:23]]
-    # x = df_refine.values #returns a numpy array
-    # standard_scaler = preprocessing.StandardScaler()
-    # x_scaled = standard_scaler.fit_transform(x)
-    # df_refine_standardize = pd.DataFrame(x_scaled)
-    # df_refine_standardize.columns=df_refine.columns
     df_refine_standardize = normalized_data(df_refine)
 
     X = df_refine_standardize.loc[:,['STAT_e', 'DISS_f10_D', 'CT_n_D_adj_An', 'CT_n_A_adj_Ca', 'DISS_wf10_D']]    
@@ -63,11 +58,6 @@ def get_input_optimal_fs(myfile):
     df_load['JSC']=df_load['JSC'].abs()
     df_reduce=df_load.iloc[:,3:]
     df_refine=df_reduce.iloc[:,np.r_[0:3,4:23]]
-    # x = df_refine.values #returns a numpy array
-    # standard_scaler = preprocessing.StandardScaler()
-    # x_scaled = standard_scaler.fit_transform(x)
-    # df_refine_standardize = pd.DataFrame(x_scaled)
-    # df_refine_standardize.columns=df_refine.columns
     df_refine_standardize = normalized_data(df_refine)
 
     X = df_refine_standardize.loc[:,['STAT_e', 'DISS_f10_D', 'CT_n_D_adj_An', 'CT_n_A_adj_Ca', 'DISS_wf10_D', 'STAT_CC_D_An', 'CT_f_A_tort1',\
